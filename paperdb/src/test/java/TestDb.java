5e3b695c185548d7c3b06dee0faa025705ce2915
kigkrazy <kigkrazy@163.com>
2019-05-15 11:16:00 +0800

    1. 去掉多余的测试文件 2. 增加路径测试

--- paperdb/src/test/java/TestDb.java
+++ paperdb/src/test/java/TestDb.java
@@ -7,5 +7,8 @@ public class TestDb {
     public void test() {
         Paper.book().write("test_string", "xxxxxxxxxxxxxxxxxxxxxxxxxxxx");
         String cc = Paper.book().read("test_string");
+
+        Paper.bookOn("d:\\tmp").write("lo_string", "xxffffggg");
+        String x2 = Paper.bookOn("d:\\tmp").read("lo_string");
     }
 }
