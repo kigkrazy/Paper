f88f06fe7fd59fc3b020baa13ba0119411667833
kigkrazy <kigkrazy@163.com>
2019-05-20 18:05:12 +0800

    UPDATE README

--- paperdb/src/test/java/TestDb.java
+++ paperdb/src/test/java/TestDb.java
@@ -1,12 +1,20 @@
 import org.junit.Test;
 
+import java.util.List;
+
 import io.paperdb.Paper;
 
 public class TestDb {
     @Test
     public void test() {
         Paper.book().write("test_string", "xxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-        String cc = Paper.book().read("test_string");
+        Paper.book().write("test_string1", "xxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        Paper.book().write("test_string2", "xxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        Paper.book().write("test_1113", "xxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        Paper.book().write("test_222", "xxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        String cc = Paper.book().read("test_string");//read the information
+        Paper.book().delete("test_string11");//read the information
+        List<String> keys = Paper.book().getAllKeys();//read the information
 
         Paper.bookOn("d:\\tmp").write("lo_string", "xxffffggg");
         String x2 = Paper.bookOn("d:\\tmp").read("lo_string");
